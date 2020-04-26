b596c9654e7166c750df0d90d1f6240ad6e510e0
cephaske254 <cephaske254@gmail.com>
2020-04-26 21:42:26 +0300

    changed importing since main/forms.py was deleterd

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
 main = Blueprint('main',__name__)
-from . import errors,forms,views
+from . import errors,views
--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,10 @@
 from flask import render_template
 from . import main
 from ..requests import get_news_articles
-from .forms import SearchForm
 
 #Views 
 @main.route('/',methods = ['GET','POST'])
 def index():
-    searchForm = SearchForm()
     general = get_news_articles('top-headlines','category=general',4)
     science = get_news_articles('top-headlines','category=science',4)
     technology = get_news_articles('top-headlines','category=technology',4)
@@ -14,4 +12,6 @@ def index():
     # health = get_news_articles('top-headlines','category=business',4)
     entertainment = get_news_articles('top-headlines','category=entertainment',4)
     business = get_news_articles('top-headlines','category=business',4)
-    return render_template('index.html',title='Home | News General',general=general,science=science,technology=technology,health=health,entertainment=entertainment,business=business,search_form=searchForm)
+    return render_template('index.html',title='Home | News General',general=general,science=science,technology=technology,health=health,entertainment=entertainment,business=business)
+
+
