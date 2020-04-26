dbfb86c247afb05a2ab89b04488a7ffb795a6f37
cephaske254 <cephaske254@gmail.com>
2020-04-27 00:13:04 +0300

    modified Testrunner

--- manage.py
+++ manage.py
@@ -7,12 +7,12 @@ manager = Manager(app)
 manager.add_command('server',Server)
 @manager.command
 def test():
-    """Run the unit tests."""
+    '''
+    Running Unittests
+    '''
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
-    # print(Config.API_KEY)
-    # print(Config.BASE_URL)
     manager.run()
