4a3500cfcffd54a30cc5c4b2f30c02ff6725fe32
cephaske254 <cephaske254@gmail.com>
2020-04-26 21:31:00 +0300

    deleted main/forms.py

--- app/main/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-
-class SearchForm(FlaskForm):
-    keywords = StringField('',validators=[Required()])
-    submit = SubmitField('Submit')
